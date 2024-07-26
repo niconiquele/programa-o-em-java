c4511123d10f39ea61598fe01e51bc51079a6c4c
niconiquele <160625409+niconiquele@users.noreply.github.com>
2024-07-26 12:04:02 -0300

    c1

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 33;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 50');
 
 // se chute for igual ao número secreto
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+ alert('acerto ta sabido(33)');
 } else {
-alert('Você errou :(')
+alert('Mó burro :(')
 }
